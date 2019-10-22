ef204e62008c2595debc9310bbbcdcf63da393fe
nhuiip <preedarat.id@hotmail.com>
2019-10-22 11:30:57 +0700

    p-[22-10-2019] // v2

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -7,6 +7,7 @@ class Project extends MX_Controller
     {
         parent::__construct();
         $this->load->model("project_model", "project");
+        $this->load->model("projectfile_model", "projectfile");
         $this->load->model("student_model", "student");
         $this->load->model("subject_model", "subject");
         $this->load->model("administrator_model", "administrator");
@@ -21,296 +22,47 @@ class Project extends MX_Controller
             $condition = array();
             $condition['fide'] = "*";
             $condition['orderby'] = "tb_project.project_id ASC";
-            $data['listdata'] = $this->project->listjoinData($condition);
+            $data['listdata'] = $this->project->listjoinData2($condition);
 
             $this->template->backend('project/main', $data);
         } else {
             $this->load->view('errors/html/error_403');
         }
-        // $poslogin   = $this->encryption->decrypt($this->input->cookie('sysp'));
-        // $idlogin    = $this->encryption->decrypt($this->input->cookie('sysli'));
-
-        // if (!empty($this->encryption->decrypt($this->input->cookie('syslev')))) {
-        //     if ($id == "") {
-        //         show_404();
-        //     } elseif ($poslogin == 'อาจารย์ผู้สอน' || $poslogin == 'หัวหน้าสาขา' || $poslogin == 'ผู้ดูแลระบบ') {
-
-        //         $condition = array();
-        //         $condition['fide'] = "use_id";
-        //         $condition['where'] = array('use_id' => $id);
-        //         $checkteacher= $this->administrator->listData($condition);
-        //         if (count($checkteacher) == 0) {
-        //             show_404();
-        //         } else {
-
-        //             $condition = array();
-        //             $condition['fide'] = "use_id,use_name";
-        //             $data['listuser'] = $this->administrator->listData($condition);
-
-        //             //============================================================//
-        //             $this->load->library('pagination');
-        //             $config['base_url'] = site_url('project/index/'.$id);
-        //             $config['total_rows'] = $this->project->count_all_news();
-        //             $config['per_page'] = 12;
-        //             $config['uri_segment'] = 3;
-        //             $choice = $config['total_rows'] / $config['per_page'];
-        //             $config['num_links'] = floor($choice);
-        //             $this->pagination->initialize($config);
-        //             $data['page'] = (!empty($_GET['page'])) ? $_GET['page'] : 0;
-
-        //             //product
-        //             $condition = array();
-        //             $condition['fide'] = "*";
-        //             $condition['limit'] = array($config['per_page'], $data['page']);
-        //             $condition['orderby'] = 'tb_project.project_create_date DESC';
-        //             $data['listproject'] = $this->project->listjoinData($condition);
-        //             $data['pagination'] = $this->pagination->create_links();
-
-        //             $condition = array();
-        //             $condition['fide'] = "*";
-        //             // $condition['orderby'] = "std_number DESC";
-        //             $data['listdata']= $this->project->listData($condition);
-
-        //             $this->template->backend('project/main', $data);
-
-        //         }
-        //     }else{
-        //         show_404();
-        //     }
-
-        // }
-
     }
 
-    public function detail($id = "", $projectId = "")
+    public function detail($id = "")
     {
-        $poslogin   = $this->encryption->decrypt($this->input->cookie('sysp'));
-        $idlogin    = $this->encryption->decrypt($this->input->cookie('sysli'));
-
-        if (!empty($this->encryption->decrypt($this->input->cookie('syslev')))) {
-            if ($id == "") {
-                show_404();
-            } else if ($poslogin == 'อาจารย์ผู้สอน' && $idlogin == $id || $poslogin == 'ผู้ดูแลระบบ' && $idlogin == $id) {
-
+        if(!empty($id)){
+            $permission = array("ผู้ดูแลระบบ", "หัวหน้าสาขา", "อาจารย์ผู้สอน");
+            if (in_array($this->encryption->decrypt($this->input->cookie('sysp')), $permission)) {
+                $data = array();
+    
                 $condition = array();
-                $condition['fide'] = "use_id";
-                $condition['where'] = array('use_id' => $idlogin);
-                $checkteacher = $this->administrator->listData($condition);
-                if (count($checkteacher) == 0) {
+                $condition['fide'] = "*";
+                $condition['where'] = array('tb_project.project_id' => $id);
+                $data['listProject'] = $this->project->listjoinData2($condition);
+                if(count($data['listProject']) != 0){
+                    $condition = array();
+                    $condition['fide'] = "*";
+                    $condition['where'] = array('tb_projectperson.project_id' => $id);
+                    $data['listPerson'] = $this->project->listperson($condition);
+
+                    $condition = array();
+                    $condition['fide'] = "*";
+                    $condition['where'] = array('tb_projectfile.project_id' => $id);
+                    $condition['orderby'] = "tb_projectfile.file_name ASC";
+                    $data['listFile'] = $this->projectfile->listjoinData($condition);
+                } else {
                     show_404();
-                } else {
-
-                    if ($projectId == "") {
-                        show_404();
-                    } else {
-
-                        $condition = array();
-                        $condition['fide'] = "project_id";
-                        $condition['where'] = array('project_id' => $projectId);
-                        $checkproject = $this->project->listData($condition);
-                        if (count($checkproject) == 0) {
-                            show_404();
-                        } else {
-
-                            $data = array();
-
-                            $condition = array();
-                            $condition['fide'] = "tb_project.project_id,tb_project.project_name,tb_project.project_status,tb_subject.sub_id,tb_subject.sub_type,tb_subject.sub_code,tb_subject.sub_name,tb_user.use_name";
-                            $condition['where'] = array('tb_project.project_id' => $projectId);
-                            $data['showproject'] = $this->project->listjoinData2($condition);
-
-
-                            $condition = array();
-                            $condition['fide'] = "*";
-                            $condition['where'] = array('project_id' => $id);
-                            $data['showproject2'] = $this->project->listData($condition);
-
-                            $this->template->backend('project/detail', $data);
-                        }
-                    }
                 }
+    
+                $this->template->backend('project/detail', $data);
             } else {
-                show_404();
-            }
-        }
-    }
-
-    public function search()
-    {
-
-        $condition = array();
-        $condition['fide'] = "use_id,use_name";
-        $data['listuser'] = $this->administrator->listData($condition);
-
-        //============================================================//
-
-        // Search text
-        if ($this->input->post('product_name') != NULL) {
-            $search_text = $this->input->post('product_name');
-        } else {
-            $search_text = "";
-        }
-
-        $this->load->library('pagination');
-        $config['base_url'] = site_url('project/index/' . $this->encryption->decrypt($this->input->cookie('sysli')));
-        $config['total_rows'] = $this->project->getrecordCount($search_text);
-        $config['per_page'] = 12;
-        $config['uri_segment'] = 3;
-        $choice = $config['total_rows'] / $config['per_page'];
-        $config['num_links'] = floor($choice);
-        $this->pagination->initialize($config);
-        $data['page'] = (!empty($_GET['page'])) ? $_GET['page'] : 0;
-
-        $users_record = $this->project->getData($data['page'], $config['per_page'], $search_text);
-        $data['pagination'] = $this->pagination->create_links();
-        $data['listproject'] = $users_record;
-
-        $this->template->backend('project/main', $data);
-    }
-
-    public function searchstatus()
-    {
-
-        $condition = array();
-        $condition['fide'] = "use_id,use_name";
-        $data['listuser'] = $this->administrator->listData($condition);
-
-        //============================================================//
-
-        $search_text = $this->input->post('type');
-
-        $this->load->library('pagination');
-        $config['base_url'] = site_url('project/index/' . $this->encryption->decrypt($this->input->cookie('sysli')));
-        $config['total_rows'] = $this->project->getrecordCountStatus($search_text);
-        $config['per_page'] = 12;
-        $config['uri_segment'] = 3;
-        $choice = $config['total_rows'] / $config['per_page'];
-        $config['num_links'] = floor($choice);
-        $this->pagination->initialize($config);
-        $data['page'] = (!empty($_GET['page'])) ? $_GET['page'] : 0;
-
-        $users_record = $this->project->getDataStatus($data['page'], $config['per_page'], $search_text);
-        $data['pagination'] = $this->pagination->create_links();
-        $data['listproject'] = $users_record;
-
-        $this->template->backend('project/main', $data);
-    }
-
-    public function searchteacher()
-    {
-
-        $condition = array();
-        $condition['fide'] = "use_id,use_name";
-        $data['listuser'] = $this->administrator->listData($condition);
-
-        //============================================================//
-
-        $search_text = $this->input->post('teacher');
-
-        $this->load->library('pagination');
-        $config['base_url'] = site_url('project/index/' . $this->encryption->decrypt($this->input->cookie('sysli')));
-        $config['total_rows'] = $this->project->getrecordCountTeacher($search_text);
-        $config['per_page'] = 12;
-        $config['uri_segment'] = 3;
-        $choice = $config['total_rows'] / $config['per_page'];
-        $config['num_links'] = floor($choice);
-        $this->pagination->initialize($config);
-        $data['page'] = (!empty($_GET['page'])) ? $_GET['page'] : 0;
-
-        $users_record = $this->project->getDataTeacher($data['page'], $config['per_page'], $search_text);
-        $data['pagination'] = $this->pagination->create_links();
-        $data['listproject'] = $users_record;
-
-        $this->template->backend('project/main', $data);
-    }
-
-    public function addproject($Id = "")
-    {
-
-        if ($Id == "") {
-            $this->load->view('errors/html/error_403');
-        } else if ($this->encryption->decrypt($this->input->cookie('sysp')) == 'นักศึกษา') {
-
-
-            $condition = array();
-            $condition['fide'] = "std_id";
-            $condition['where'] = array('std_id' => $Id);
-            $checkstudent = $this->student->listData($condition);
-            if (count($checkstudent) == 0) {
                 $this->load->view('errors/html/error_403');
-            } else {
-
-                if ($this->tokens->verify('formcrfaddproject')) {
-
-                    if ($this->input->post('radioInline3') == 1) {
-                        $teacherId  =   $this->input->post('teacher_id');
-                    } else {
-                        $teacherId  =   $this->input->post('txt_teacher');
-                    }
-
-                    //================================================================ check radio [1=เดี่ยว, 2=กลุ่ม]
-                    if ($this->input->post('radioInline') == 1) {
-
-                        $data = array(
-                            'project_name'          => $this->input->post('txt_projectname'),
-                            'use_id'                => $teacherId,
-                            'std_id'                => $this->input->post('Idstd'),
-                            'project_status'        => '1',
-                            'project_create_name'   => $this->encryption->decrypt($this->input->cookie('sysn')),
-                            'project_create_date'   => date('Y-m-d H:i:s'),
-                            'project_lastedit_name' => $this->encryption->decrypt($this->input->cookie('sysn')),
-                            'project_lastedit_date' => date('Y-m-d H:i:s'),
-                        );
-
-                        $this->project->insertData($data);
-                    } else {
-                        $data = array(
-                            'project_name'          => $this->input->post('txt_projectname'),
-                            'use_id'                => $teacherId,
-                            'std_id'                => $this->input->post('Idstd'),
-                            'project_status'        => '1',
-                            'project_create_name'   => $this->encryption->decrypt($this->input->cookie('sysn')),
-                            'project_create_date'   => date('Y-m-d H:i:s'),
-                            'project_lastedit_name' => $this->encryption->decrypt($this->input->cookie('sysn')),
-                            'project_lastedit_date' => date('Y-m-d H:i:s'),
-                        );
-
-                        $Id_project  =   $this->project->insertData($data);
-
-
-                        $other = array();
-                        for ($i = 0; $i < count($this->input->post('txt_std_id')); $i++) {
-                            $condition = array();
-                            $condition['fide'] = "*";
-                            $condition['where'] = array('project_id' => $Id_project);
-                            $checkproject = $this->project->listData($condition);
-
-                            $idstd  =   $checkproject[0]['std_id'];
-
-                            $other['project_id']     = $Id_project;
-                            $other['std_id']         = $idstd . ',' . $this->input->post('txt_std_id')[$i];
-
-                            $this->project->updateData2($other);
-                        }
-                    }
-                    //================================================================ ./check radio
-
-                    $result = array(
-                        'error' => false,
-                        'msg' => 'เพิ่มข้อมูลเรียบร้อยแล้ว',
-                        'url' => site_url('project/index/' . $Id)
-                    );
-                    echo json_encode($result);
-                } else {
-                    $result = array(
-                        'error' => true,
-                        'title' => "ล้มเหลว",
-                        'msg' => "อัพเดตข้อมูลไม่สำเร็จ"
-                    );
-
-                    echo json_encode($result);
-                }
             }
+        } else {
+            show_404();
         }
+        
     }
 }
--- application/views/project/main.php
+++ application/views/project/main.php
@@ -3,7 +3,7 @@
     <div class="col-lg-12">
         <h2>ข้อมูลปริญญานิพนธ์</h2>
         <ol class="breadcrumb">
-            <li><a href="<?= site_url('project/index/' . $this->encryption->decrypt($this->input->cookie('sysli'))); ?>">หน้าแรก</a></li>
+            <li><a href="<?= site_url('dashboard/index');?>">หน้าแรก</a></li>
             <li class="active"><strong>ข้อมูลปริญญานิพนธ์</strong></li>
         </ol>
     </div>
@@ -21,67 +21,95 @@
                 <div class="ibox-content">
                     <!-- table ------------------------------------------------------------------------------------------------------->
                     <div class="table-responsive">
-                        <table class="table table-striped table-bordered table-hover dataTables-export" data-colexport="0,1,2,3,4" data-filename="export-project" width="100%">
+                        <table class="table table-striped table-bordered table-hover dataTables-export" data-colexport="1,2,3,4,5" data-filename="export-project" width="100%">
                             <thead>
                                 <tr>
+                                    <th></th>
                                     <th>รหัสปริญญานิพนธ์</th>
                                     <th>ชื่อปริญญานิพนธ์</th>
+                                    <th>อาจารย์ที่ปรึกษา</th>
                                     <th>ผู้จัดทำปริญญานิพนธ์</th>
-                                    <th>สถานะปริญญานิพนธ์</th>
-                                    <th>วันที่สร้าง</th>
+                                    <th>
+                                        <center>สถานะปริญญานิพนธ์</center>
+                                    </th>
+                                    <th></th>
+                                    <th class="none">วันที่สร้าง</th>
+                                    <th class="none">แก้ไขล่าสุด</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?PHP foreach ($listdata as $key => $value) { ?>
-                                    <tr class="gradeX">
-                                        <td width="5%" style="vertical-align:top"><strong><?= "PRO" . str_pad($value['project_id'], 5, "0", STR_PAD_LEFT); ?></strong></td>
-                                        <td width="10%" style="vertical-align:top"><?= $value['project_name'] ?></td>
-                                        <td width="15%" style="vertical-align:top">
-                                            <?PHP
-                                                if ($value['std_id']  != '') {
-                                                    $stypesVal = explode(",", $value['std_id']);
-                                                } else {
-                                                    $stypesVal = '';
-                                                }
+                                <?PHP foreach ($listdata as $key => $value) {
+                                    $this->db->select('*');
+                                    $this->db->from('tb_projectperson');
+                                    $this->db->join('tb_student', 'tb_student.std_id = tb_projectperson.std_id');
+                                    $this->db->join('tb_project', 'tb_project.project_id = tb_projectperson.project_id');
+                                    $this->db->where(array('tb_projectperson.project_id' => $value['project_id']));
+                                    $query = $this->db->get();
+                                    $projectperson = $query->result_array();
+                                    switch ($value['project_status']) {
+                                        case 0:
+                                            $status_text = '<span class="badge badge-danger">&nbsp;&nbsp;ยกเลิกโปรเจค&nbsp;&nbsp;</span>';
+                                            break;
+                                        case 1:
+                                            $status_text = '<span class="badge">&nbsp;&nbsp;เริ่มต้น&nbsp;&nbsp;</span>';
+                                            break;
+                                        case 2:
+                                            $status_text = '<span class="badge badge-primary">&nbsp;&nbsp;ผ่านโครงการสารสนเทศ 1&nbsp;&nbsp;</span>';
+                                            break;
+                                        case 3:
+                                            $status_text = '<span class="badge badge-warning">&nbsp;&nbsp;ติดแก้ไขโครงการสารสนเทศ 2&nbsp;&nbsp;</span>';
+                                            break;
+                                        case 4:
+                                            $status_text = '<span class="badge badge-primary">&nbsp;&nbsp;ผ่านโครงการสารสนเทศ 2&nbsp;&nbsp;</span>';
+                                            break;
+                                        case 5:
+                                            $status_text = '<span class="badge badge-info">&nbsp;&nbsp;ผ่านโครงการสารสนเทศ 2 (conference)&nbsp;&nbsp;</span>';
+                                            break;
+                                    }
+                                    ?>
+                                        <tr class="gradeX">
+                                            <td width="1%"></td>
+                                            <td width="9%"><strong><?= "PRO" . str_pad($value['project_id'], 3, "0", STR_PAD_LEFT); ?></strong></td>
+                                            <td width="30%"><?= $value['project_name'] ?></td>
+                                            <td width="15%"><?= $value['use_name']; ?></td>
+                                            <td width="25%">
+                                                <?PHP foreach ($projectperson as $key => $list) { ?>
+                                                    <span class="badge">&nbsp;&nbsp;<?= $list['std_title'] . '' . $list['std_fname'] . ' ' . $list['std_lname']; ?>&nbsp;&nbsp;</span>
+                                                <? } ?>
+                                            </td>
+                                            <td width="15%">
+                                                <center><?= $status_text ?></center>
+                                            </td>
+                                            <td width="5%">
+                                                <center class="tooltip-demo">
+                                                    <a href="<?= site_url('project/detail/'.$value['project_id']); ?>">
+                                                        <button class="btn btn-default" data-toggle="tooltip" data-placement="top" title="ข้อมูลเพิ่มเติม">
+                                                            <i class="fa fa-chevron-right"></i>
+                                                        </button>
+                                                    </a>
+                                                </center>
+                                            </td>
+                                            <td>
+                                                <?= $value['project_create_name']; ?>
+                                                <small class="text-muted"><i class="fa fa-clock-o"></i> <?= date('d/m/Y h:i A', strtotime($value['project_create_date'])); ?></small>
+                                            </td>
+                                            <td>
+                                                <?= $value['project_lastedit_name']; ?>
+                                                <small class="text-muted"><i class="fa fa-clock-o"></i> <?= date('d/m/Y h:i A', strtotime($value['project_lastedit_date'])); ?></small>
+                                            </td>
 
-                                                if (count($stypesVal) != 0) {
-                                                    foreach ($stypesVal as $key => $values) {
-
-                                                        $this->db->select('std_id,std_number,std_title,std_fname,std_lname');
-                                                        $this->db->where(array('std_id' => $values));
-                                                        $query_student = $this->db->get('tb_student');
-                                                        $liststudent = $query_student->result_array();
-                                                        ?>
-                                                    <div class="form-group">
-                                                        <label><?= $liststudent[0]['std_number'] ?> <?= $liststudent[0]['std_title'] ?><?= $liststudent[0]['std_fname'] ?> <?= $liststudent[0]['std_lname'] ?></label>
-                                                    </div>
-                                            <?PHP
-                                                    }
-                                                }
-                                                ?>
-                                        </td>
-                                        <td width="10%" style="vertical-align:top">
-                                            <?PHP if ($value['project_status'] == 1) { ?>
-                                                ยังไม่สอบโครงงานหนึ่ง
-                                            <?PHP } else if ($value['project_status'] == 2) { ?>
-                                                ผ่านโครงงานหนึ่ง
-                                            <?PHP } else if ($value['project_status'] == 3) { ?>
-                                                สอบโครงงานสองแล้วติดแก้ไข
-                                            <?PHP } else if ($value['project_status'] == 4) { ?>
-                                                สอบโครงงานสองผ่าน
-                                            <?PHP } else if ($value['project_status'] == 0) { ?>
-                                                เปลี่ยนหัวข้อปริญญานิพนธ์
-                                            <?PHP } ?>
-                                        </td>
-                                        <td width="10%" style="vertical-align:top"><?= $value['project_create_name'] ?><br /><?= $value['project_create_date'] ?></td>
-                                    </tr>
-                                <?PHP } ?>
+                                        </tr>
+                                    <?PHP } ?>
                             </tbody>
                             <tfoot>
                                 <tr>
                                     <th></th>
+                                    <th></th>
                                     <th class="ftinput">ปริญญานิพนธ์</th>
-                                    <th class="ftinput">ชื่อ-สกุล นักศึกษา</th>
+                                    <th class="ftinput">อาจารย์ที่ปรึกษา</th>
+                                    <th class="ftinput">ผู้จัดทำ</th>
+                                    <th class="ftinput">สถานะปริญญานิพนธ์</th>
+                                    <th></th>
                                     <th></th>
                                     <th></th>
                                 </tr>
